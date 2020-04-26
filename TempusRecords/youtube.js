5ace0f57917bc607e532c87fe582ecea15907f01
laurirasanen <laurirasanen@occasionally.cool>
2020-04-26 19:59:55 +0300

    Add option for not deleting compressed videos

--- TempusRecords/youtube.js
+++ TempusRecords/youtube.js
@@ -1,40 +1,37 @@
-﻿const handbrake = require('handbrake-js'),
-    youtube_api = require('youtube-api'),
-    ffmpegPath = require('@ffmpeg-installer/ffmpeg').path,
-    ffmpeg = require('fluent-ffmpeg'),
-    fs = require('fs'),
-    Lien = require('lien'),
-    prettyBytes = require('pretty-bytes'),
-    config = require('./config.json'),
-    opn = require('opn'),
-    utils = require('./utils.js');
+﻿const handbrake = require("handbrake-js"),
+    youtube_api = require("youtube-api"),
+    ffmpegPath = require("@ffmpeg-installer/ffmpeg").path,
+    ffmpeg = require("fluent-ffmpeg"),
+    fs = require("fs"),
+    Lien = require("lien"),
+    prettyBytes = require("pretty-bytes"),
+    config = require("./config.json"),
+    opn = require("opn"),
+    utils = require("./utils.js");
 
 let server = new Lien({
     host: "localhost",
-    port: "5000"
+    port: "5000",
 });
 
-let oauth = youtube_api.authenticate(
-    {
-        type: "oauth",
-        client_id: config.youtube.client_id,
-        client_secret: config.youtube.client_secret,
-        redirect_url: config.youtube.redirect_url
-    });
+let oauth = youtube_api.authenticate({
+    type: "oauth",
+    client_id: config.youtube.client_id,
+    client_secret: config.youtube.client_secret,
+    redirect_url: config.youtube.redirect_url,
+});
 
-opn(oauth.generateAuthUrl({
-    access_type: "offline",
-    scope: ["https://www.googleapis.com/auth/youtube"]
-}));
+opn(
+    oauth.generateAuthUrl({
+        access_type: "offline",
+        scope: ["https://www.googleapis.com/auth/youtube"],
+    })
+);
 
-server.addPage("/oauth2callback", lien =>
-{
+server.addPage("/oauth2callback", (lien) => {
     console.log("Trying to get the token using the following code: " + lien.query.code);
-    oauth.getToken(lien.query.code, (err, tokens) =>
-    {
-        if (err)
-        {
-
+    oauth.getToken(lien.query.code, (err, tokens) => {
+        if (err) {
             lien.end(err, 400);
             return console.log(err);
         }
@@ -47,49 +44,41 @@ server.addPage("/oauth2callback", lien =>
     });
 });
 
-function compress(file, cb)
-{
-    if (!cb || typeof (cb) !== 'function')
-        throw 'callback is not a function';
+function compress(file, cb) {
+    if (!cb || typeof cb !== "function") throw "callback is not a function";
 
     console.log(`Compressing ${file}`);
 
-    handbrake.spawn(
-        {
-            "input": file,
-            "output": `${file.split(".avi")[0]}_compressed.mp4`,
-            "encoder": "x264",
-            "vb": "68000",
+    handbrake
+        .spawn({
+            input: file,
+            output: `${file.split(".avi")[0]}_compressed.mp4`,
+            encoder: "x264",
+            vb: "68000",
             "two-pass": true,
             "encoder-profile": "high",
             "encoder-level": "4.2",
             "encoder-preset": "veryfast",
             "encoder-tune": "film",
-            "rate": 60,
-            "width": 3840,
-            "height": 2160,
-            "aencoder": "copy:aac"
+            rate: 60,
+            width: 3840,
+            height: 2160,
+            aencoder: "copy:aac",
         })
-        .on('error', err =>
-        {
+        .on("error", (err) => {
             throw err;
         })
-        .on('progress', progress =>
-        {
+        .on("progress", (progress) => {
             console.log(`${file}: Compressed: ${progress.percentComplete}, ETA: ${progress.eta}`);
         })
-        .on('cancel', () =>
-        {
+        .on("cancel", () => {
             return cb(false, null);
         })
-        .on('complete', () =>
-        {
+        .on("complete", () => {
             // Remove uncompressed video
-            fs.unlink(file, err =>
-            {
-                if (err)
-                {
-                    console.log('Failed to unlink uncompressed recording');
+            fs.unlink(file, (err) => {
+                if (err) {
+                    console.log("Failed to unlink uncompressed recording");
                     console.log(err);
                 }
             });
@@ -98,53 +87,45 @@ function compress(file, cb)
         });
 }
 
-function remux(video, audio, output, cb)
-{
+function remux(video, audio, output, cb) {
     ffmpeg.setFfmpegPath(ffmpegPath);
     ffmpeg()
         .input(video)
         .input(audio)
-        .videoCodec('copy')
-        .outputOptions(['-map 0:v:0', '-map 1:a:0'])
+        .videoCodec("copy")
+        .outputOptions(["-map 0:v:0", "-map 1:a:0"])
         .save(output)
-        .on("error", (err, stdout, stderr) =>
-        {
+        .on("error", (err, stdout, stderr) => {
             console.log(err.message);
         })
-        .on('end', (stdout, stderr) =>
-        {
-            console.log('Remux done');
+        .on("end", (stdout, stderr) => {
+            console.log("Remux done");
 
             // Remove old video and audio files
-            fs.unlink(video, err =>
-            {
-                if (err)
-                {
-                    console.log('Failed to unlink remux input video');
+            fs.unlink(video, (err) => {
+                if (err) {
+                    console.log("Failed to unlink remux input video");
                     console.log(err);
                     return;
                 }
 
-                console.log('Unlinked remux input video');
+                console.log("Unlinked remux input video");
             });
-            fs.unlink(audio, err =>
-            {
-                if (err)
-                {
-                    console.log('Failed to unlink remux input audio');
+            fs.unlink(audio, (err) => {
+                if (err) {
+                    console.log("Failed to unlink remux input audio");
                     console.log(err);
                     return;
                 }
 
-                console.log('Unlinked remux input audio');
+                console.log("Unlinked remux input audio");
             });
 
             return cb(true, output);
         });
 }
 
-function upload(file, demo)
-{
+function upload(file, demo) {
     console.log(`Uploading ${file}`);
 
     var description = "";
@@ -152,11 +133,11 @@ function upload(file, demo)
     var fileSize = stats.size;
     var bytes = 0;
 
-    config.youtube.description.forEach(line =>
-    {
+    config.youtube.description.forEach((line) => {
         var d = new Date();
         var demo_date = new Date(demo.demo_info.date * 1000);
-        line = line.replace("$MAP_URL", 'https://tempus.xyz/maps/' + demo.map.name)
+        line = line
+            .replace("$MAP_URL", "https://tempus.xyz/maps/" + demo.map.name)
             .replace("$MAP", demo.map.name)
             .replace("$NAME", demo.player_info.name)
             .replace("$TIME", utils.secondsToTimeStamp(demo.duration))
@@ -164,67 +145,54 @@ function upload(file, demo)
             .replace("$DATETIME", d.toUTCString())
             .replace("$DATE", demo_date.toUTCString())
 
-            .replace("$DEMO_URL", 'https://tempus.xyz/demos/' + demo.demo_info.id);
+            .replace("$DEMO_URL", "https://tempus.xyz/demos/" + demo.demo_info.id);
 
         description += line + "\n";
     });
 
     // Common tags for all videos
-    var tags = [
-        "Team Fortress 2",
-        "TF2",
-        "rocketjump",
-        "speedrun",
-        "tempus",
-        "record"
-    ];
+    var tags = ["Team Fortress 2", "TF2", "rocketjump", "speedrun", "tempus", "record"];
 
     // Video specific tags
     tags.push(demo.player_info.name);
     var mapParts = demo.map.name.split("_");
     tags.push(...mapParts);
-    if (mapParts.length > 1)
-        tags.push(`${mapParts[0]}_${mapParts[1]}`);
+    if (mapParts.length > 1) tags.push(`${mapParts[0]}_${mapParts[1]}`);
     tags.push(demo.class === 3 ? ["soldier", "solly"] : ["demoman", "demo"]);
 
     var req = youtube_api.videos.insert(
         {
-            resource:
-            {
-                snippet:
-                {
-                    title: config.youtube.title.replace("$NAME", demo.player_info.name).replace("$MAP", demo.map.name).replace("$TIME", utils.secondsToTimeStamp(demo.duration)),
+            resource: {
+                snippet: {
+                    title: config.youtube.title
+                        .replace("$NAME", demo.player_info.name)
+                        .replace("$MAP", demo.map.name)
+                        .replace("$TIME", utils.secondsToTimeStamp(demo.duration)),
                     description: description,
-                    tags: tags
+                    tags: tags,
                 },
-                status:
-                {
+                status: {
                     privacyStatus: "private",
-                    publishAt: new Date(Date.now() + 1000 * 60 * 30).toISOString() // Allow for 30 mins of processing before making public
-                }
+                    publishAt: new Date(Date.now() + 1000 * 60 * 30).toISOString(), // Allow for 30 mins of processing before making public
+                },
             },
             // This is for the callback function
             part: "snippet,status",
 
             // Create the readable stream to upload the video
-            media:
-            {
-                    body: fs.createReadStream(file).on('data', (chunk) => {
-                        bytes += chunk.length;
-                        console.log(`${file}: ${prettyBytes(bytes)} (${(100 * bytes / fileSize).toFixed(2)}%) uploaded.`);
-                    })
-            }
+            media: {
+                body: fs.createReadStream(file).on("data", (chunk) => {
+                    bytes += chunk.length;
+                    console.log(`${file}: ${prettyBytes(bytes)} (${((100 * bytes) / fileSize).toFixed(2)}%) uploaded.`);
+                }),
+            },
         },
-        (err, data) =>
-        {
-            if (err)
-            {
+        (err, data) => {
+            if (err) {
                 console.log("Failed to upload video");
                 console.log(err);
                 return;
-            }
-            else
-            {
+            } else {
                 console.log("Done uploading");
             }
 
@@ -233,67 +201,63 @@ function upload(file, demo)
                 {
                     resource: {
                         snippet: {
-                            playlistId: demo.class === 3 ? "PL_D9J2bYWXyLFs5OJcTugl_70HqzDN9nv" : "PL_D9J2bYWXyIeRkUq099oCV8wf5Omf9Fe",
+                            playlistId:
+                                demo.class === 3
+                                    ? "PL_D9J2bYWXyLFs5OJcTugl_70HqzDN9nv"
+                                    : "PL_D9J2bYWXyIeRkUq099oCV8wf5Omf9Fe",
                             resourceId: {
                                 kind: "youtube#video",
-                                videoId: data.id
-                            }
-                        }
+                                videoId: data.id,
+                            },
+                        },
                     },
-                    part: "snippet"
+                    part: "snippet",
                 },
-                (err, data) =>
-                {
-                    if (err)
-                    {
+                (err, data) => {
+                    if (err) {
                         console.log("Failed to add video to playlist");
                         console.log(err);
-                    }
-                    else
-                    {
+                    } else {
                         console.log("Video added to playlist");
                     }
-                });
+                }
+            );
 
             // Update last uploaded timestamp
-            utils.readJson('./uploaded.json', (err, uploaded) =>
-            {
-                if (err !== null)
-                {
-                    console.log('Failed to read last uploaded');
+            utils.readJson("./uploaded.json", (err, uploaded) => {
+                if (err !== null) {
+                    console.log("Failed to read last uploaded");
                     console.log(err);
                     return;
                 }
 
-                if (!uploaded.maps.includes(demo.id))
-                    uploaded.maps.push(demo.id);
+                if (!uploaded.maps.includes(demo.id)) uploaded.maps.push(demo.id);
 
-                utils.writeJson('./uploaded.json', uploaded, (err) =>
-                {
-                    if (err !== null)
-                    {
-                        console.log('Failed to write last uploaded');
+                utils.writeJson("./uploaded.json", uploaded, (err) => {
+                    if (err !== null) {
+                        console.log("Failed to write last uploaded");
                         console.log(err);
                         return;
                     }
 
-                    console.log('Updated uploaded list');
+                    console.log("Updated uploaded list");
                 });
 
                 // Remove compressed video
-                fs.unlink(file, err =>
-                {
-                    if (err)
-                    {
-                        console.log('Failed to unlink uploaded video');
-                        console.log(err);
-                        return;
-                    }
+                if (config.youtube.delete_compressed) {
+                    fs.unlink(file, (err) => {
+                        if (err) {
+                            console.log("Failed to unlink uploaded video");
+                            console.log(err);
+                            return;
+                        }
 
-                    console.log('Unlinked uploaded video');
-                });
+                        console.log("Unlinked uploaded video");
+                    });
+                }
             });
-        });
+        }
+    );
 }
 
 module.exports.compress = compress;
