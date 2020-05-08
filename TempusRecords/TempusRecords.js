ffca0120a81097b08e3e0c76239fe359e2e17619
laurirasanen <laurirasanen@occasionally.cool>
2020-05-08 21:05:41 +0300

    Sanity check input when uploading a specific run

--- TempusRecords/TempusRecords.js
+++ TempusRecords/TempusRecords.js
@@ -17,7 +17,20 @@ function start() {
         if (process.argv.length > 3) {
             recent = false;
             mapName = process.argv[2];
+
             className = process.argv[3];
+
+            // Remembering input format is hard...
+            if (!mapName.startsWith("jump_")) {
+                mapName = "jump_" + mapName;
+            }
+            className = className
+                .replace("demo", "d")
+                .replace("demoman", "d")
+                .replace("solly", "s")
+                .replace("soldier", "s")
+                .replace("3", "s")
+                .replace("4", "d");
         }
     }
 
